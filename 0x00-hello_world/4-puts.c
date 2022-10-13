11b0924974ec20c8b37e9a4166593d8e2ec40a81
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-13 06:16:38 -0700

    source code of task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** 
+/**
  * main - prints the str "Programming is like building a multilingual puzzle
- *  Return : 0 if exited properly, non-zero otherwise  
+ * Return: 0 it doesn't return something
 */
 int main(void)
 {
