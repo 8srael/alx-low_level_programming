c8c23e73ea0a98555b25d09b93cea40acd895630
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-13 08:21:30 -0700

    source codes of advanced task 7 and 8 updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
- * main -  prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error .
+ * main -  prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error .
  *
+ * The program should compile without any warnings when using the -Wall gcc option
  * we used "write" function of the library "<unistd.h>"
  *
- * Return: 1
- * The program should compile without any warnings when using the -Wall gcc option
+ * Return: always 1 (Success)
 */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
