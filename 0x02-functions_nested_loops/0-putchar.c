0faa7922a4170eb852f5259354a8e54cabfd155e
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 09:27:52 -0700

    code source of task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar", followed by a new line.
+ * 
+ *
+ * Return: 0 if program runs succesfully.
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
