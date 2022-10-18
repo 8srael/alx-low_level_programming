1f01af5185cbe895742e3a84e593929e377d4bdd
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 11:49:29 -0700

    header file updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,23 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 /**
  * _putchar - writes the character c
  * @c: parameter that you want to print
  * Return: 1 or 0
 */
 int _putchar(char c);
+
 /**
- *  print_alphabet - function to print alphabet
- *
+ *  print_alphabet - function that prints alphabet in lowercase
+ *  followed by a new line
 */
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ * followed by a new line
+*/
+void print_alphabet_x10(void);
+
 #endif
