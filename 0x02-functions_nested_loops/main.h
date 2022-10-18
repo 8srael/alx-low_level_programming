adfaed12def4125db70bd6d2f78b977226c9f79f
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 12:12:25 -0700

    header file updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,11 @@ void print_alphabet(void);
 */
 void print_alphabet_x10(void);
 
+/**
+ * _islower - function that checks for lowercase character
+ * @c : parameter that you want to check
+ * Return: 1 if c is lowercase , 0 otherwise
+*/
+int _islower(int c);
+
 #endif
