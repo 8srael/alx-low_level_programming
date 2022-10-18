348ccb86dea2e3a0a36754a9cc4c7309de0541e7
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 10:05:18 -0700

    header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c
+ * @c: parameter that you want to print
+ * Return: 1 or 0
+*/
+int _putchar(char c);
+/**
+ *  print_alphabet - function to print alphabet
+ *
+*/
+void print_alphabet(void);
+#endif
