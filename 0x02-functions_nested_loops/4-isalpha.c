be0f060b31a5ac89e766c04d49dcddc646d0f638
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 12:35:18 -0700

    source code of task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c : character that you want to check
+ *
+ * Return: 1 if c is a letter,
+ * lowercase or uppercase,
+ * 0 otherwise.
+*/
+
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c >= 65 && c < 90))
+		return (1);
+	else
+		return (0);
+}
