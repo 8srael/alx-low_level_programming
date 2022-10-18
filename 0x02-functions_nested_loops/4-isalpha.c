c423f8fd6d0422324d32abc707756ef15770c515
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 12:36:34 -0700

    source code of task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,7 @@
  * _isalpha - checks for alphabetic character.
  * @c : character that you want to check
  *
- * Return: 1 if c is a letter,
- * lowercase or uppercase,
+ * Return: 1 if c is a letter, lowercase or uppercase
  * 0 otherwise.
 */
 
