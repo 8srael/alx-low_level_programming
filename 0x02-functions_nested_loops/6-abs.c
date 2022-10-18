ae87a2fe32beea2190731e02cb65910d1922ca5a
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 13:30:46 -0700

    source code of task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n : number that you want to know his absolute value
+ *
+ * Return: absolute value of n.
+*/
+
+int _abs(int n)
+{
+	return ((n > 0) ? n : -n);
+}
