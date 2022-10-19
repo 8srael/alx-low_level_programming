01387fc8bf9a7623fafc97902d27910328752e2f
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-19 14:09:10 -0700

    header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,11 +3,6 @@
 
 #include <stdio.h>
 
-/**
- * positive_or_negative - function that prints if integer
- * is positive or negative
- * @i : param
- */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
