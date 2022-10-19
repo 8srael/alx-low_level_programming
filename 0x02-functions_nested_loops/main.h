b14a99510894c5aaa3afcc0a552f8186cfc19f17
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-18 17:31:59 -0700

    header file updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -76,4 +76,15 @@ void times_table(void);
  * Return: the sum of a and b
 */
 int add(int a, int b);
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98,
+ * followed by a new line.
+ * Numbers must be separated by a comma, followed by a space
+ * Numbers should be printed in order
+ * The first printed number should be the number passed to your function
+ * The last printed number should be 98.
+ * @n : parameter
+*/
+void print_to_98(int n);
 #endif
