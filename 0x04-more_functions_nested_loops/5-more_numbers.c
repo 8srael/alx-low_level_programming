a422e462e183bc3cce7dc043290d6d986b07a6a8
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 09:49:00 -0700

    source code of task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, 
+ * more_numbers - prints 10 times the numbers,
  * from 0 to 14, followed by a new line
  * by using the function _putchar in main.h
  *
@@ -11,15 +11,15 @@
 void more_numbers(void)
 {
 	int i;
-	char n;
+	char x;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (n = 0 ; n < 15 ; n++)
+		for (x = 0 ; x < 15 ; x++)
 		{
-			if(n / 10 != 0)
-				_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			if (x / 10 != 0)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 		}
 		 _putchar('\n');
 	}
