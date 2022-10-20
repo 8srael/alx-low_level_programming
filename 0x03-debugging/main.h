5c8724a2d8707763e1562584d8da5fbe33caf569
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 04:04:38 -0700

    header file updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,20 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+/**
+ * convert_day - converts days
+ * @month : month
+ * @day : day
+ * Return: converted day
+*/
 int convert_day(int month, int day);
+
+/**
+ *  print_remaining_days - prints year in day
+ *  @month : month
+ *  @day : day
+ *  @year : year
+*/
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
