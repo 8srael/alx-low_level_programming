7e060f259cdb64adf2871966d7c840eaf6d62c59
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 05:22:42 -0700

    header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,17 @@
 
 /**
  * _isupper - function that checks for uppercase character
- * @c : parameter that we want to check
+ * @c : character that we want to check
  * Return: 1 if c is uppercase , 0 otherwise
 */
 int _isupper(int c);
+
+/**
+ * _isupper - function that checks for a digit
+ * (0 through 9)
+ * @c : character that we want to check
+ * Return: 1 if c is a digit , 0 otherwise
+*/
+int _isdigit(int c);
 
 #endif
