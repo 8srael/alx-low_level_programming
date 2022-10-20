bcefba5e0bdb1a0fbd1a541de0f60254023da6b1
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 02:57:18 -0700

    last advanced task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,41 @@
 int main(void)
 {
 	int count = 0;
+	int overflow;
 
 	unsigned long n1 = 1;
-	unsigned long n2 = 2;
-	unsigned long n3;
+	unsigned long n2 = 1;
+	unsigned long n3 = 0;
 
-	printf("%lu, ", n1);
-	printf("%lu, ", n2);
-	for (count = 2 ; count < 98 ; count++)
+	long n1_head, n1_tail, n2_head, n2_tail, n3_head, n3_tail;
+
+	printf("%lu", n1);
+
+	for (count = 2 ; count < 93 ; count++)
 	{
 		n3 = n1 + n2;
-		printf("%lu", n3);
 		n1 = n2;
 		n2 = n3;
-		if (count != 97)
-			printf(", ");
+		printf(", %lu", n3);
+	}
+
+	n1_head = n1 / 1000000000;
+	n2_tail = n2 % 1000000000;
+	n1_head = n1 / 1000000000;
+	n2_tail = n2 % 1000000000;
+
+	for (count = 93 ; count < 99 ; count++)
+	{
+		overflow = (n1_tail + n2_tail) / 1000000000;
+		n3_head = (n1_head + n2_head) + overflow;
+		n3_tail = (n1_tail + n2_tail) - (1000000000 * overflow);
+
+		printf(", %lu%lu", n3_head, n3_tail);
+
+		n1_head = n2_head;
+		n1_tail = n2_tail;
+		n2_head = n3_head;
+		n2_tail = n3_tail;
 	}
 
 	printf("\n");
