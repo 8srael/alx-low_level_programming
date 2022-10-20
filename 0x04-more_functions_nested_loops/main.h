bb1ef81f993370af1c8dfb52b3b72bf2aa3c5ae7
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 05:06:09 -0700

    header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c : parameter that we want to check
+ * Return: 1 if c is uppercase , 0 otherwise
+*/
+int _isupper(int c);
+
+#endif
