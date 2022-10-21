57d9ac7945b6e80faf734b5d07ca98fb2f34c017
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-20 17:28:45 -0700

    source code of first advanced task , task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor 
+ * of the number 612852475143, followed by a new line.
+ *
+ * Return: 0 if program runs successfuly
+*/
+
+int main(void)
+{
+	unsigned long number = 612852475143;
+	unsigned long prime = 2;
+
+	while (prime < number)
+	{
+		if (number % prime == 0)
+		{
+			number /= prime;
+			prime = 2 ;
+		}
+		else
+			prime++;
+	}
+	printf("%lu\n",number);
+
+	return (0);
+}
