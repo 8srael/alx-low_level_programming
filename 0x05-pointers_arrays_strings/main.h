55b6b39fb960b0b6aaac5905ca6c3964870dd9e9
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-24 00:39:11 -0700

    header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c
+ * @c: parameter that you want to print
+ * Return: 1 or 0
+*/
+int _putchar(char c);
+
+/**
+ *  reset_to_98 - function that takes a pointer to an int as parameter
+ *  and updates the value it points to to 98.
+ *  @n : pointer to an int
+ */
+void reset_to_98(int *n);
+#endif
