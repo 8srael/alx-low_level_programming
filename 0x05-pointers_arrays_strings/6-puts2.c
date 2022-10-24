bad97551fdaf9f97577e9ee0c502c680b0f3e36b
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-24 14:54:05 -0700

    source code of task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 -  prints every other character of a string
+ * starting with the first character
+ *
+ * @str : string (array of char)
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+	int len = _strlen(str);
+
+	for (i = 0 ; i < len ; i += 3)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
