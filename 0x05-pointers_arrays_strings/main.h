481d55d40f92cceb9cd25d1b1d405ec198ed9c7f
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-24 00:52:57 -0700

    header file updated

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,4 +14,12 @@ int _putchar(char c);
  *  @n : pointer to an int
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ *
+ * @a : pointer to an int
+ * @b : pointer to an int
+ */
+void swap_int(int *a, int *b);
 #endif
