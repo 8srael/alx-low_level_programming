03f10cb76c9132595e12f27aa02b394e0d6ddfb6
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-26 23:48:14 -0700

    header file updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -58,4 +58,14 @@ char *_strncat(char *dest, char *src, int n);
  */
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ * _strcmp - function that comapres two strings
+ *
+ * @s1 : first string
+ * @s2 : second string
+ *
+ * Return: an integer positive or negative
+ */
+int _strcmp(char *s1, char *s2);
+
 #endif
