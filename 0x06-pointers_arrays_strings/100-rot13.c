114b417afe22603d467e245f9bce1e7d3c275522
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-28 11:33:47 -0700

    source code of advanced task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rot13 - function that encrypts code
+ * rot13 - function that encryptsstring using rot13
  *
  * @s : string
  *
@@ -10,23 +10,45 @@
 
 char *rot13(char *s)
 {
-	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
-
 	int i;
-	int j;
 
 	for (i = 0 ; *(s + i) != '\0' ; i++)
 	{
-		for (j = 0 ; part1[j] != '\0' ; j++)
-		{
-			if (s[i] == part1[j])
-			{
-				s[i] = part2[j];
-				break;
-			}
-		}
+		*(s + i) = rot13_char(*(s + i));
 	}
 
 	return (s);
+}
+
+/**
+ * rot13_char - function that encodes a char using rot13
+ *
+ * @c : a character
+ *
+ * Return: character encrypted
+ */
+
+char rot13_char(char i)
+{
+	int c = (int)i;
+
+	if (c >= 97 && c <= 122)
+	{
+		c = c + 13;
+		if (c > 122)
+		{
+			c = c - 122;
+			c = 97 + c - 1;
+		}
+	}
+	if (c >= 65 && c <= 90)
+	{
+		c = c + 13;
+		if (c > 90)
+		{
+       			c = c - 90;
+			c = 65 + c - 1;
+		}
+	}
+	return (c);
 }
