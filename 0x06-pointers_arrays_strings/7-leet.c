fb203d75d3bb1bf4beba1d828ab94ac68e25d82c
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-27 23:31:54 -0700

    source code of task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - function that encrypts code
+ * leet - function that encodes a string into 1337
  *
  * @s : string
  *
- * Return: string encrypted
+ * Return: string encoded
 */
 
-char *rot13(char *s)
+char *leet(char *s)
 {
-	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char alpha1337[] = "a4A4e3E3o0O0t7T7l1L1";
 
 	int i;
-	int j = 0;
+	int j;
 
 	for (i = 0 ; *(s + i) != '\0' ; i++)
 	{
-		for (j = 0 ; part1[j] != '\0' ; j++)
+		for (j = 0 ; alpha1337[j] != '\0' ; j++)
 		{
-			if (s[i] == part1[j])
+			if (s[i] == alpha1337[j])
 			{
-				s[i] = part2[j];
+				s[i] = alpha1337[j + 1];
 				break;
 			}
 		}
