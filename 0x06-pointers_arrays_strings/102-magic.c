78a05193450d433e7df9e5ce1187f1058dab71cc
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-27 23:45:35 -0700

    magic task ! advanced task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,4 @@
-#include "main.h"
 #include <stdio.h>
-
-/**
- * main - magic code !!!
- *
- * Return: Always 0.
-*/
 
 int main(void)
 {
@@ -22,7 +15,7 @@ int main(void)
 	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
-	*/
+	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
