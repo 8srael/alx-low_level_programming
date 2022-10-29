cdaa4a8cbb155db88b75eb82f5e51c4dc56360f6
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-28 23:17:42 -0700

    source code of advanced task 12 , last task

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include "2-strlen.c"
+#include <stdio.h>
+
+/**
+ * print_buffer - function that print a buffer
+ *
+ * @b: buffer to print
+ * @size : buffer size
+ */
+
+void print_buffer(char *b, int size)
+{
+	int x;
+
+	for (x = 0; x <= (size - 1) / 10 && size; x++)
+	{
+		printf("%08x: ", x * 10);
+
+		if (x < size / 10)
+			print_line(b, 9, x);
+		else
+			print_line(b, (size % 10) - 1, x);
+
+		printf("\n");
+	}
+
+	if (size == 0)
+		printf("\n");
+}
+
+/**
+ * print_line - function that print size bytes of a buffer
+ *
+ * @b: buffer to print
+ * @size : bytes of buffer to print
+ * @line : line of buffer to print
+ */
+
+void print_line(char *b, int size, int line)
+{
+	int y, z;
+
+	for (y = 0 ; y < 10 ; y++)
+	{
+		if (y <= size)
+			printf("%02x", *(b + line * 10 + y));
+		else
+			printf("  ");
+		if (y % 2 == 0)
+			putchar(' ');
+	}
+
+	for (z = 0; z < size + 1 ; z++)
+	{
+		if (*(b + line * 10 + z) > 31 && *(b + line * 10 + z) < 127)
+			putchar(*(b + line * 10 + z));
+		else
+			putchar('.');
+	}
+}
