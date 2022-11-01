5d6147eff45af19a93464a7bf70287b220b8d248
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-31 21:15:29 -0700

    source code of task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strpbrk - function that searches a string for any of a set of bytes.
@@ -6,8 +7,7 @@
  * @s : a string
  * @accept : a string
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept,
- * or NULL if no such byte is found
+ * Return: a pointer to the byte
 */
 
 char *_strpbrk(char *s, char *accept)
