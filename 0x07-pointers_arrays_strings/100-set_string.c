d013f16cdf09a5fcf5e773078f706ece5906493c
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-31 22:35:05 -0700

    advanced task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - function that prints the chessboard.
+ *
+ * @s : pointer to a pointer
+ * @to : string to set
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
