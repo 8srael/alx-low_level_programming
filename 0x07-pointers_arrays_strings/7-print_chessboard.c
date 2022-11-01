2f270c211109564dfc818cc9275a3f487f77d59b
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-01 11:29:31 -0700

    source code of task 6 ,chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,12 +10,8 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	int len = 0;
 
-	while (**(a + len) != '\0')
-		len++;
-
-	for (i = 0 ; i < len - 1 ; i++)
+	for (i = 0 ; i < 8 ; i++)
 	{
 		for (j = 0 ; j < 8 ; j++)
 			_putchar(a[i][j]);
