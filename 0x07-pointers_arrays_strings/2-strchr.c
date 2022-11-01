eb48909e0a441d955e92b1ca633fc1452da950ef
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-31 18:58:01 -0700

    source code of task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s : a string
+ * @c : a character
+ *
+ * Return: a pointer to the first occurence of c in s, NULL if c isn't found
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i, j;
+	char *first;
+
+	i = 0;
+
+	for (i = 0 ; *(s + i) != '\0' && *(s + i) != c ; i++)
+	{}
+
+	if (i == 0)
+		first = NULL;
+	else
+		first = malloc(i * sizeof(char));
+	j = 0;
+
+	for (j = i ; *(s + j) != '\0' ; j++)
+		*(first + j - i) = *(s + j);
+
+	return (first);
+}
