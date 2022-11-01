a48f93884082cf893853e13edd1f0019df280ddc
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-31 17:45:47 -0700

    source code of task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest : a pointer to a destination memory address
+ * @src : a pointer to a source memory address
+ * @n : number of bytes that we want to copy
+ *
+ * Return: a pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
