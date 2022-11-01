f3d9d7cd2fba9a3e7b42321ebcfad0c4ad4df502
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-10-31 17:26:34 -0700

    source code of first task, task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ *
+ * @s : a pointer to a memory address
+ * @b : constant byte that we want to fill
+ * @n : number of bytes
+ *
+ * Return: a pointer to s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s);
+}
