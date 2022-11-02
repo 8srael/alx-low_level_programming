6b98b9693d2077637bd6997f0f1f287e24d55605
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-02 13:27:27 -0700

    header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,27 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c
+ *
+ * @c: parameter that you want to print
+ * Return: 1 or 0
+*/
+int _putchar(char c);
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s : string (array of char)
+ *
+ * Return: the length of the string in param
+ */
+int _strlen(char *s);
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ *
+ * @s : string to print
+ */
+void _puts_recursion(char *s);
+#endif
