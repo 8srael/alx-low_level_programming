6fb4d413d5aac86163db78e50d51ad2a3adb768c
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-04 11:45:20 -0700

    source code of task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc : number of arguments supplied to the prog
+ * argc is an integer
+ * @argv : array of pointers to the strings which are those args
+ * argv is an array of pointers of char (string array)
+ *
+ * Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(*(argv + argc - 1));
+	n2 = atoi(*(argv + argc - 2));
+
+	printf("%d\n", n1 * n2);
+
+	return (0);
+}
