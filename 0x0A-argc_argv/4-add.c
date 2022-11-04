69cfe70608442b725a8976ce1c3c0e5a6acad3dc
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-04 12:28:47 -0700

    source code of task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,6 +19,12 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
 	for (i = 1 ; i < argc ; i++)
 	{
 		if (*(argv + i)[0] >= 58 || *(argv + i)[0] <= 47)
