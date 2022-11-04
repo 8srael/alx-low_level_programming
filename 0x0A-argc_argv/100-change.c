ab8f039d89ec0a290936996ba7fc3bdd10358bf3
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-04 14:53:29 -0700

    source code of the only advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,28 +14,36 @@
 
 int main(int argc, char *argv[])
 {
-	int n, coins = 0;
+	int cash, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (argv[1][0] == '-')
+
+	if (*(argv + 1)[0] == '-')
 	{
 		printf("0\n");
 		return (0);
 	}
-	n = atoi(argv[1]);
-	coins += n / 25;
-	n = n % 25;
-	coins += n / 10;
-	n = n % 10;
-	coins += n / 5;
-	n = n % 5;
-	coins += n / 2;
-	n = n % 2;
-	coins += n / 1;
+
+	cash = atoi(*(argv + 1));
+
+	coins += cash / 25;
+	cash = cash % 25;
+
+	coins += cash / 10;
+	cash = cash % 10;
+
+	coins += cash / 5;
+	cash = cash % 5;
+
+	coins += cash / 2;
+	cash = cash % 2;
+
+	coins += cash / 1;
+
 	printf("%d\n", coins);
 	return (0);
 }
