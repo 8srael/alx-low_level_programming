09d5a5c22967f2518a510892a3a1df9f3a21826b
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-08 12:32:51 -0800

    source code of task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that creates a grid
+ *
+ * @width : width of the grid
+ * @height : height of the grid
+ *
+ * Return:  returns a pointer to a 2 dimensional array of integers
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+	int i;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < height ; i++)
+		*(grid + i) = (int *)malloc(width * sizeof(int));
+
+	return (grid);
+}
