c621c59c82a1cfee329345ccc292d92359eecb82
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-07 16:33:19 -0800

    source code of the third task, task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	fullSize = strlen(s1) + strlen(s2) ;
+	fullSize = strlen(s1) + strlen(s2) + 1;
 
 	final_string = (char *) malloc((sizeof(char) * fullSize));
 
