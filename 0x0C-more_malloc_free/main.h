5c716fe40df1baa528f02c6f457c4c537aa2f01c
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-10 13:29:21 -0800

    source code of first task, task 0

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c
+ *
+ * @c: parameter that you want to print
+ * Return: 1 or 0
+*/
+int _putchar(char c);
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s : string (array of char)
+ *
+ * Return: the length of the string in param
+ */
+int _strlen(char *s);
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ *
+ * @b : size
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+#endif
