c487cdcae4cc2800fa649cde471b3d6c4a4391a3
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-14 15:21:40 -0800

    source code of first task, task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - definition of a struct dog
+ *
+ * @name : name of the dog (string)
+ * @age : dog's age (float number)
+ * @owner : dog's owner (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
