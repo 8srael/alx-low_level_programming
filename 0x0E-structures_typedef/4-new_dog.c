311fa81f59a52e45e1155e41b8e4f059601b39cf
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-14 22:27:07 -0800

    source code of task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(strlen(name) * sizeof(char));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(name) ; i++)
+		*(dog->name + i) = *(name + i);
+
+	dog->age = age;
+
+	dog->owner = malloc(strlen(owner) * sizeof(char));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(owner); i++)
+		*(dog->owner + i) = *(owner + i);
+
+	return (dog);
+}
