ad7ed32b5351c1e751b0752b1ab320dd5f0e0713
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-14 16:18:06 -0800

    dog header file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,41 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ *
+ * @d : struct that we wnat to initialize
+ * @name : string
+ * @age : float number
+ * @owner : array of char
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - function that prints a struct dog
+ *
+ * @d : struct that we want to print
+ */
+void print_dog(struct dog *d);
+
+
+typedef struct dog dog_t;
+
+/**
+ * new_dog - function that creates a new dog
+ * @name : string
+ * @age : float number
+ * @owner : array of char
+ *
+ * Return: NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d : pointer to a struct that we wnat to free
+ */
+void free_dog(dog_t *d);
+
 #endif
