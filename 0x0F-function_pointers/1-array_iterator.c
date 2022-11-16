7ca628b3a2f817d6abc62770f77503497babbd59
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-16 13:09:28 -0800

    Second task updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,6 +17,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array != NULL && action != NULL)
 	{
 		for (x = 0 ; x < size ; x++)
-			action(*(array + i));
+			action(*(array + x));
 	}
 }
