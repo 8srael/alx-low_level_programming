082fe836574eccc411f111862aa52ca04fc9252c
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 15:53:05 -0800

    first task , task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
@@ -11,7 +12,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	int i = 0;
 
 	while (h)
 	{
@@ -20,9 +21,9 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			count++;
+			i++;
 			h = h->next;
 		}
 	}
-	return (count);
+	return (i);
 }
