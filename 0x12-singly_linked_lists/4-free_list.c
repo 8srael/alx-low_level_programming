abe9e73b63c6d7f4069c186d67f3866e8a72c7cf
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 17:43:11 -0800

    source code task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - function that frees a list_t list.
+ *
+ * @head: head of a list_t list.
+ */
+
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+
+		free(head);
+	}
+}
