7c829a17acfeb07afae02b65edffdcc841736a66
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 16:23:48 -0800

    second task , task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,14 +4,14 @@
 #include <stddef.h>
 
 /**
- * list_len - shows 
+ * list_len - shows
  * @h: Pointer to a linked list head.
  * Return: number of elements in a linked list_t list.
  */
 
 size_t list_len(const list_t *h)
 {
-	int number_of_all_elements;
+	int number_of_all_elements = 0;
 
 	while (h)
 	{
