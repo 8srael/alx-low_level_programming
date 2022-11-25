4ec2ee88b64e7517f43e2f2a6187c0ad925b22ce
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 16:57:23 -0800

    third task , task 2

---
+++ 0x12-singly_linked_lists/2-add_note.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - new node at the beginning of a list_t list.
+ * @head: head of a list_t list.
+ * @str: value to insert into element.
+ *
+ * Return: number of elements in a linked list_t list.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *add;
+
+	add = malloc(sizeof(list_t));
+
+	if (add == NULL)
+		return (NULL);
+
+	add->str = strdup(str);
+	add->len = strlen(str);
+	add->next = *head;
+
+	head = add;
+
+	return (add);
+}
