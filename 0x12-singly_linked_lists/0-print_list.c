1ddb8c16560f342b4811784224a7d428c5173e47
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 16:00:35 -0800

    first task , task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
  * print_list - prints all of the elements of a linked list.
  * @h: Pointer to a linked list head.
@@ -19,11 +17,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			i++;
-			h = h->next;
-		}
+		i++;
+		h = h->next;
 	}
 	return (i);
 }
