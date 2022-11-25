c0b6a26c85517e3564f0b33bfdb83aed4021309b
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 17:07:54 -0800

    third tadk

--- 0x12-singly_linked_lists/2-add_note.c
+++ 0x12-singly_linked_lists/2-add_note.c
@@ -5,7 +5,7 @@
 
 /**
  * add_node - new node at the beginning of a list_t list.
- * @head: head of a list_t list.
+ * @*head: head of a list_t list.
  * @str: value to insert into element.
  *
  * Return: number of elements in a linked list_t list.
@@ -22,10 +22,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	add->str = strdup(str);
+
 	add->len = strlen(str);
 	add->next = *head;
 
-	head = add;
+	*head = add;
 
 	return (add);
 }
