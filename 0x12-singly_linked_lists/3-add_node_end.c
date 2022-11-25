0744677166d5c4a905493dd45be65799c202b9b4
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-24 17:36:18 -0800

    source code task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = _strlen(new->str);
+	new->len = strlen(new->str);
 	new->next = NULL;
 	if (*head == NULL)
 	{
