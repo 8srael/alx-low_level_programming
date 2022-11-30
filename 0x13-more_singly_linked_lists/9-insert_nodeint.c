9ba5d539dafa5cbfa5a3a7d467d3143a83641f24
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-29 21:31:03 -0800

    task 9 updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		temp = *head;
-		for (j = 0; i < idx - 1 && temp != NULL; j++)
+		for (j = 0; j < idx - 1 && temp != NULL; j++)
 			temp = temp->next;
 
 		if (!temp)
