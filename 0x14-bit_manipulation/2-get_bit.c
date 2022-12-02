589b029acbfa137eb97b71f4113c3a0e5b8d7846
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-12-02 14:06:25 -0800

    source code of task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,15 +10,16 @@
  *
  * Return: the value of the bit at index
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int div, res;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
+
 	div = 1 << index;
 	res = n & div;
+
 	if (res == div)
 		return (1);
 
