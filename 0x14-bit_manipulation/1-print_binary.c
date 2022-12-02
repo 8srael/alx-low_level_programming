adfb06d81c6bfb00ec4c5365f498e7aaa77ac79d
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-12-02 12:54:36 -0800

    task 1 updated

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _power - function that calculates (base and power)
+ *
+ * @b : base
+ * @p : power
+ *
+ * Return: value of base and power
+ */
+
+unsigned long int _power(unsigned int b, unsigned int p)
+{
+	unsigned long int number = 1;
+	unsigned int i;
+
+	for (i = 1 ; i <= pow ; i++)
+		num *= base;
+
+	return (number);
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int x, result;
+	char flag = 0;
+
+	x = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (x != 0)
+	{
+		result = n & x;
+		if (result == x)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || x == 1)
+		{
+			_putchar('0);
+		}
+
+		x >>= 1;
+	}
+}
