ea0ca3466b2d2c00c32689a6bff6e05f76583d5a
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-12 22:39:35 -0800

    task 1 , number of elements in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in the linked list
+ *
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len;
+
+	for (len = 0; len != NULL; len++)
+		h = h->next;
+
+	return (len);
+}
