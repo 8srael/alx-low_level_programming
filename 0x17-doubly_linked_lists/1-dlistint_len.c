3a1b43550387053348edd4746e9a39b875710006
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-01-12 22:45:29 -0800

    task 1 , number of elements in a list , an error fixed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,10 +9,10 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len;
+	size_t length;
 
-	for (len = 0; len != NULL; len++)
+	for (length = 0; h != NULL; length++)
 		h = h->next;
 
-	return (len);
+	return (length);
 }
